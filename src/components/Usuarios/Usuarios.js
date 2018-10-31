c5603f9da6886c0caabfc56b3112322811d34f3a
Pablo <pgrajeda@betalnk.com>
2018-10-30 21:01:02 -0600

    Cambio de encabezados a tabla usuarios y correcion de diseño en titulo usuarios

--- src/components/Usuarios/Usuarios.js
+++ src/components/Usuarios/Usuarios.js
@@ -13,37 +13,39 @@ class Usuarios extends Component {
 
             <div>
                 <Menu></Menu>
-                <h4 className="border-b m-20 pb-5"> 
-                    Usuarios
-                    <Button floating large className='green accent-4' waves='light' icon='add' />
-                </h4>
 
                 <div className="usuarios-container">
+                    <h4 className="usuarios-title border-b m-20 pb-5"> 
+                        Usuarios
+                        <Button floating className='green accent-4 mlr-15' waves='light' icon='add' />
+                    </h4>
                     <Card className="usuarios-card">
                         <Table>
                             <thead>
                                 <tr>
-                                <th data-field="id">Name</th>
-                                <th data-field="name">Item Name</th>
-                                <th data-field="price">Item Price</th>
+                                <th data-field="name">Nombre</th>
+                                <th data-field="name">Apellido Paterno</th>
+                                <th data-field="first-surname">Apellido Materno</th>
+                                <th data-field="second-surname">Edad</th>
+                                <th data-field="action">Acciones</th>
                                 </tr>
                             </thead>
 
                             <tbody>
                                 <tr>
-                                <td>Alvin</td>
-                                <td>Eclair</td>
-                                <td>$0.87</td>
+                                    <td>Alvin</td>
+                                    <td>Eclair</td>
+                                    <td>$0.87</td>
                                 </tr>
                                 <tr>
-                                <td>Alan</td>
-                                <td>Jellybean</td>
-                                <td>$3.76</td>
+                                    <td>Alan</td>
+                                    <td>Jellybean</td>
+                                    <td>$3.76</td>
                                 </tr>
                                 <tr>
-                                <td>Jonathan</td>
-                                <td>Lollipop</td>
-                                <td>$7.00</td>
+                                    <td>Jonathan</td>
+                                    <td>Lollipop</td>
+                                    <td>$7.00</td>
                                 </tr>
                             </tbody>
                         </Table>
